4128d6159543adaf9f7b5dbf93d67d3513f7ebf0
tomasr <lezamatomas@hotmail.com>
2023-08-23 17:38:27 -0300

    className X class

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const CartWidget = () => {
   return (
     <a href="#" className='cart-container'>
-        <i class="bi bi-cart3"></i>
+        <i className="bi bi-cart3"></i>
         <p className='cart-num'>0</p>
     </a>
   )
